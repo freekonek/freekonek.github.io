a8a0b7e69a1aa18fc715313365f432539008cd6e
freekonek <166968786+freekonek@users.noreply.github.com>
2024-04-14 14:51:37 +0800

    Update flash.php

--- flash.php
+++ flash.php
@@ -1,7 +1,7 @@
 #!/bin/sh
 fw_setenv bootdelay 5
 wget https://raw.github.com/freekonek/freekonek.io/main/a.bin -O /tmp/firmware.bin
-sleep1
+sleep 1
 wget https://raw.github.com/freekonek/freekonek.io/main/uboot.bin -O /tmp/uboot.bin
 sleep 1
 fw_setenv bootargs console=ttyS1,57600n8 root=/dev/mtdblock5
