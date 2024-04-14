e4d6ef3d04b64d31fdfcd6c8d1faaffb0928c586
freekonek <166968786+freekonek@users.noreply.github.com>
2024-04-14 14:14:36 +0800

    Update flash.php

--- flash.php
+++ flash.php
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-EXPECTED_HASH_A="fb4e2077419dedf612f1a036ae4d83d7"  # Expected hash value for a.bin
+EXPECTED_HASH_FIRMWARE="fb4e2077419dedf612f1a036ae4d83d7"  # Expected hash value for firmware.bin
 EXPECTED_HASH_UBOOT="d1e62ee1b49e7c7567a967db819ac531"  # Expected hash value for uboot.bin
 
 check_hash() {
@@ -15,17 +15,17 @@ check_hash() {
 
 fw_setenv bootdelay 5
 
-wget http://raw.github.com/freekonek/freekonek.io/main/a.bin -O /tmp/a.bin
+wget http://raw.github.com/freekonek/freekonek.io/main/a.bin -O /tmp/firmware.bin
 sleep 1
 wget http://raw.github.com/freekonek/freekonek.io/main/uboot.bin -O /tmp/uboot.bin
 sleep 1
 
-check_hash "/tmp/a.bin" "$EXPECTED_HASH_A"
+check_hash "/tmp/firmware.bin" "$EXPECTED_HASH_FIRMWARE"
 check_hash "/tmp/uboot.bin" "$EXPECTED_HASH_UBOOT"
 
 fw_setenv bootargs console=ttyS1,57600n8 root=/dev/mtdblock5
 sleep 1
 
-mtd write /tmp/a.bin /dev/mtd4
+mtd write /tmp/firmware.bin /dev/mtd4
 mtd write /tmp/uboot.bin /dev/mtd1
 jffs2reset -y && sleep 3 && reboot
