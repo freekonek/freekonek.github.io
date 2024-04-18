9deefee1998eea1e567117c90fa12b730b751589
freekonek <166968786+freekonek@users.noreply.github.com>
2024-04-18 20:57:56 +0800

    Update flash.php

--- flash.php
+++ flash.php
@@ -1,41 +1,45 @@
 #!/bin/sh
 
-# Set boot delay to 5 seconds
+# Set bootdelay to 5 seconds
 fw_setenv bootdelay 5
 
-# Download firmware binary using wget
-wget http://freekonek.github.io/r051-stock-12-3.bin -O /tmp/firmware.bin
-
-#Check if download was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to download firmware."
-    exit 1
-fi
-
-# Calculate checksum of downloaded firmware
-checksum=$(md5sum /tmp/firmware.bin | awk '{print $1}')
-
-# Compare checksum with expected value
-expected_checksum="fb4e2077419dedf612f1a036ae4d83d7"
-if [ "$checksum" != "$expected_checksum" ]; then
-    echo "Checksum verification failed. Firmware may be corrupted."
-    exit 1
-fi
-
-# Download uboot binary using wget
+# Download firmware files
 wget http://freekonek.github.io/uboot.bin -O /tmp/uboot.bin
-
-# Check if download was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to download uboot binary."
-    exit 1
-fi
+sleep 1
+wget http://freekonek.github.io/r051-with-qos.bin -O /tmp/firmware.bin
+sleep 1
 
 # Set boot arguments
 fw_setenv bootargs console=ttyS1,57600n8 root=/dev/mtdblock5
+sleep 1
 
-# Write uboot binary to memory device
+# Write uboot binary to partition
+echo "Writing uboot binary to partition..."
 mtd write /tmp/uboot.bin /dev/mtd1
 
-# Write firmware to memory device, reset JFFS2 file system, and reboot
-mtd write /tmp/firmware.bin /dev/mtd4 && jffs2reset -y && sleep 3 && reboot
+# Check firmware hash
+firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
+echo "Checking hash!"
+hash=$(md5sum /tmp/firmware.bin | awk '{print $1}')
+expected_hash="86b00ec51f178242483bba656dfcacc0"
+
+if [ "$hash" == "$expected_hash" ]; then
+    echo "Same!"
+    echo "Installing Bands 8 and 38..."
+    echo "Installing Band and PCI locking features..."
+    echo "Installing Change IMEI and Openline features..."
+    echo "Firmware upgrading in process..."
+
+    # Check firmware partition and upgrade firmware
+    if [ "$firmware2" == "mtd7:" ]; then
+        echo "Wait for the modem to reboot..."
+        mtd -r write /tmp/firmware.bin /dev/mtd4
+        exit
+    else
+        echo "Wait for the modem to reboot..."
+        mtd -r write /tmp/firmware.bin /dev/mtd5
+        exit
+    fi
+else
+    echo "Not same!"
+fi
