ec097d5f63d2fb63c835626a90a2274141aaaf53
freekonek <166968786+freekonek@users.noreply.github.com>
2024-04-18 20:34:04 +0800

    Update r051-smartbro.sh

--- r051-smartbro.sh
+++ r051-smartbro.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-jffs2reset -y > /dev/null 2>&1
+jffs2reset -y
 fw_setenv dropbear_mode
 fw_setenv dropbear_password
 fw_setenv dropbear_key_type
-wget http://freekonek.github.io/r051-smartbro-8bands.bin -O /tmp/a.bin
+wget http://freekonek.github.io/r051-smartbro-8bands.bin -O /tmp/firmware.bin
 firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
 echo "Checking hash!"
-hash=$(md5sum /tmp/a.bin | awk '{print $1}')
+hash=$(md5sum /tmp/firmware.bin | awk '{print $1}')
 echo "$hash = 86b00ec51f178242483bba656dfcacc0"
 if [ $hash == '86b00ec51f178242483bba656dfcacc0' ]
 then echo "Same!"
@@ -16,11 +16,11 @@ echo "Installing Change IMEI and Openline features..."
 echo "Firmware upgrading on process..."
 if [ $firmware2 == 'mtd7:' ];
 then echo "Wait for the modem to reboot..."
-mtd -r write /tmp/a.bin /dev/mtd4
+mtd -r write /tmp/firmware.bin /dev/mtd4
 exit
 fi
 echo "Wait for the modem to reboot..."
-mtd -r write /tmp/a.bin /dev/mtd5
+mtd -r write /tmp/firmware.bin /dev/mtd5
 exit
 else
 echo "Not same!" 
