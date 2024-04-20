2a69d10ad505f84d674e0fe5560d176f2882222a
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-04-20 12:11:13 +0800

    Update flash.sh

--- flash.php
+++ flash.php
@@ -6,7 +6,7 @@ fw_setenv bootdelay 5
 # Download firmware files
 wget http://freekonek.github.io/uboot.bin -O /tmp/uboot.bin
 sleep 1
-wget http://freekonek.github.io/r051-with-qos.bin -O /tmp/firmware.bin
+wget http://freekonek.github.io/r051-stock-12-3.bin -O /tmp/firmware.bin
 sleep 1
 
 # Set boot arguments
@@ -21,7 +21,7 @@ mtd write /tmp/uboot.bin /dev/mtd1
 firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.bin | awk '{print $1}')
-expected_hash="86b00ec51f178242483bba656dfcacc0"
+expected_hash="92674cdcc9fb8eb3e145577568c93622"
 
 if [ "$hash" == "$expected_hash" ]; then
     echo "Same!"
