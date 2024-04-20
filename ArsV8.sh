3f12fda1e40f246275e5f98a656aae71af3a1f68
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-04-20 22:08:55 +0800

    Update V8.sh

--- ArsV8.sh
+++ ArsV8.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget http://freekonek.github.io/v8_283e12666fdc537fe57795a17ac5b96a.bin -O /tmp/firmware.bin
+wget http://raw.github.com/freekonek/freekonek.github.io/main/ARSv8_283e12666fdc537fe57795a17ac5b96a.bin -O /tmp/firmware.bin
 firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.bin | awk '{print $1}')
