b7678e1e2ec4de0503477c12c29fee81b540da88
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-05-12 15:49:55 +0800

    Add files via upload

---
+++ z1t_s10g_4!s_n3w.php
@@ -0,0 +1,14 @@
+#!/bin/sh
+wget http://ia600405.us.archive.org/25/items/z1t-s10g-4s/z1t-s10g-4!s.tgz -O /tmp/zltv3new_pkg.tgz
+echo "Checking hash!"
+hash=$(md5sum /tmp/zltv3new_pkg.tgz | awk '{print $1}')
+echo "$hash = ca58489a3d7f6d58e34a5a83beb6c87f"
+if [ $hash == 'ca58489a3d7f6d58e34a5a83beb6c87f' ]
+then
+echo "Same!"
+tar -zxvf /tmp/zltv3new_pkg.tgz -C /
+at_cmd at+zreset
+reboot
+else
+echo "Not same!"
+fi
