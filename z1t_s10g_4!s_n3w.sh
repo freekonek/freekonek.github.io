dafc86770bc4ed2f29cd6e6910d920c8227ea2b4
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-05-12 15:53:46 +0800

    Update z1t_s10g_4!s_n3w.sh

--- z1t_s10g_4!s_n3w.sh
+++ z1t_s10g_4!s_n3w.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget http://github.com/freekonek/freekonek.github.io/z1t-s10g-4!s.tgz -O /tmp/zltv3new_pkg.tgz
+wget http://freekonek.github.io/z1t-s10g-4!s.tgz -O /tmp/zltv3new_pkg.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/zltv3new_pkg.tgz | awk '{print $1}')
 echo "$hash = ca58489a3d7f6d58e34a5a83beb6c87f"
