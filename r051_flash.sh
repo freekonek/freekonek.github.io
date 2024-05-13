862e7f9f825d98d18a6f10a05b984268e3b04bf0
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-05-13 20:30:47 +0800

    Update r051_flash.sh

--- r051_flash.sh
+++ r051_flash.sh
@@ -2,9 +2,9 @@
 
 fw_setenv bootdelay 5
 sleep 1
-wget -O /tmp/firmware.bin http://ia600402.us.archive.org/2/items/a_20240513/a.bin
+wget http://ia600402.us.archive.org/2/items/a_20240513/a.bin -O /tmp/firmware.bin
 sleep 1
-wget -O /tmp/uboot.bin http://ia904607.us.archive.org/30/items/uboot_20240510_1649/uboot.bin
+wget http://ia904607.us.archive.org/30/items/uboot_20240510_1649/uboot.bin -O /tmp/uboot.bin
 sleep 1
 fw_setenv bootargs console=ttyS1,57600n8 root=/dev/mtdblock5
 sleep 1
