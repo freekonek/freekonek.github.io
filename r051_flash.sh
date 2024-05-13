6ba2f74426b17c368f4add2356d10a875db35d1a
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-05-13 20:16:54 +0800

    Add files via upload

---
+++ r051_flash.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+fw_setenv bootdelay 5
+wget -O /tmp/firmware.bin http://freekonek.github.io/r051-with-qos.bin
+sleep 1
+wget -O /tmp/uboot.bin http://freekonek.github.io/uboot.bin
+sleep 1
+fw_setenv bootargs console=ttyS1,57600n8 root=/dev/mtdblock5
+sleep 1
+mtd write /tmp/uboot.bin /dev/mtd1
+sleep 3
+mtd write /tmp/firmware.bin /dev/mtd4 && jffs2reset -y && sleep 3
+reboot
