e79ad18a8fa5f441427588138cf596b1dc145ed4
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-06-13 03:52:03 +0800

    Update AIS.sh

--- AIS.sh
+++ AIS.sh
@@ -3,9 +3,9 @@ at_cmd at+modimei="012936005672168"
 wget https:///freekonek.github.io/aisV2.tgz -O /tmp/firmware.tgz 
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
-echo "$hash = 8ead29179e6a33279e8857a9e90e0efa"
+echo "$hash = ca58489a3d7f6d58e34a5a83beb6c87f"
 if
-[ $hash == '8ead29179e6a33279e8857a9e90e0efa' ]
+[ $hash == 'ca58489a3d7f6d58e34a5a83beb6c87f' ]
 then
 echo "Same!"
 tar -zxvf /tmp/firmware.tgz -C /
