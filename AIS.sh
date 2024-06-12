9856c8fe9f8dc9990d9bb16bc712e766392f2861
freekonek_92 <166968786+freekonek@users.noreply.github.com>
2024-06-13 03:47:46 +0800

    Update AIS.sh

--- AIS.sh
+++ AIS.sh
@@ -1,1 +1,16 @@
-
+#!/bin/sh 
+at_cmd at+modimei="012936005672168"
+wget http://juliussimega.shop/files/zltais-fw.tgz -O /tmp/firmware.tgz 
+echo "Checking hash!"
+hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
+echo "$hash = 8ead29179e6a33279e8857a9e90e0efa"
+if
+[ $hash == '8ead29179e6a33279e8857a9e90e0efa' ]
+then
+echo "Same!"
+tar -zxvf /tmp/firmware.tgz -C /
+at_cmd at+zreset
+reboot
+else
+echo "Not same!"
+fi
